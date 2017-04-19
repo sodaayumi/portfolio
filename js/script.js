1226f179d18403a3b22c9b1bf2e19b6eec23bcee
ayunmi <sodaayumi1@gmail.com>
2017-04-19 16:26:09 +0900

    jsつけてtopへ戻るの表示を徐々に変化

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
+//トグルメニューの動き
 $(function(){
-    $("#navBtn").click(function(){
-       $("#gnav").stop().slideToggle(600) 
+    $("#gnav-btn").click(function(){
+        $(".gnav-ul").stop().slideToggle(600) ;
+        return false;
     });
 });
+
+//TOPへ戻るの動き
+$("#top-modoru").click(function () {
+    $("html,body").animate({
+        scrollTop: 0
+    }, 1000);
+    return false;
+});
+
+//メインビジュアルのスライドする動き
+var count = $("#slide li").length;
+var current = 1;
+var next = 2;
+
+timer = setInterval(slideTimer, 3000);
+
+//function slideTimer(){
+//    $("#slide li:nth-child(+" + current + ")").fadeOut(800);
+//    $("#slide li:nth-child(+" + next + ")").fadeIn(800);
+//
+//    current = next;
+//    next = ++next;
+//
+//    if(next > count){
+//        next = 1;
+//    }
+//} 
